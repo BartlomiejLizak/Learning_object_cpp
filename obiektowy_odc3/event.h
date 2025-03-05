6de77628bea3297fc8047603946eba6fb5c6e506
Bartlomiejlizak <Wiazix@gmail.com>
2025-03-05 21:00:34 +0100

    Dodano odcinek 3 mz

---
+++ obiektowy_odc3/event.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Event
+{
+    int day, month, year;
+    int hour,minutes;
+    string name;
+
+    public:
+    Event(string = "brak",int = 1,int = 1,int = 2025,int = 12,int =00);
+    ~Event();
+    void load();
+    void show();
+};
